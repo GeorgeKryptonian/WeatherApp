60e14c325f334835beb47a07c437ad13634e2d47
GeorgeKryptonian <georgekryptonian@gmail.com>
2023-01-05 20:26:10 +0100

    "getNextFourDays" module has been added, minor updates to index.html and existing modules

--- src/scripts/modules/getByCityName.js
+++ src/scripts/modules/getByCityName.js
@@ -1,9 +1,13 @@
-export function getByCityName(value) {
-    return require('axios').get('https://api.openweathermap.org/data/2.5/weather', {
+import {getNextFourDays} from "./getNextFourDays";
+
+//? Makes API call to OpenWeatherMap with specified type (forecast or weather) and city name, and returns either next four days' forecast or current weather data.
+export function getByCityName(type, value) {
+    return require('axios').get(`https://api.openweathermap.org/data/2.5/${type}`, {
         params: {
             q: value,
+            cnt: 40,
             units: 'metric',
             appid: '8257a26fa136769d0cee0bd2caa0a891'
         }
-    }).then(((response) => response.data));
+    }).then(((response) => (type === 'forecast') ? getNextFourDays(response.data) : response.data));
 }
--- src/scripts/modules/getByCurrentCoordinates.js
+++ src/scripts/modules/getByCurrentCoordinates.js
@@ -1,10 +1,14 @@
-export function getByCurrentCoordinates(latitude, longitude) {
-    return require('axios').get('https://api.openweathermap.org/data/2.5/weather', {
+import {getNextFourDays} from "./getNextFourDays";
+
+//? Makes API call to OpenWeatherMap with specified type (forecast or weather) and user's current coordinates, and returns either next four days' forecast or current weather data.
+export function getByCurrentCoordinates(type, latitude, longitude) {
+    return require('axios').get(`https://api.openweathermap.org/data/2.5/${type}`, {
         params: {
             lat: latitude,
             lon: longitude,
+            cnt: 40,
             units: 'metric',
             appid: '8257a26fa136769d0cee0bd2caa0a891'
         }
-    }).then(((response) => response.data));
+    }).then(((response) => (type === 'forecast') ? getNextFourDays(response.data) : response.data));
 }
